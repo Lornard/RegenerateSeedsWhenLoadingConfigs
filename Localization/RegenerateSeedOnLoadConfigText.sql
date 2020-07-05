4ad46952198d57f91bdb77ead5cf0e1813a34afa
Lornard <gabriel.silveira@gmail.com>
2020-07-04 22:01:12 -0300

    First Commit

---
+++ Localization/RegenerateSeedOnLoadConfigText.sql
@@ -0,0 +1,12 @@
+-- GameData1
+-- Author: Gabriel2
+-- DateCreated: 7/2/2020 9:11:17 PM
+--------------------------------------------------------------
+INSERT OR IGNORE INTO LocalizedText
+		(Language,	Tag,														Text)
+VALUES	('en_US',	'LOC_LOAD_CONFIG_REGENERATE_SEED_WARNING',					'Do you wish to generate a new game seed for this game?');
+
+
+INSERT OR IGNORE INTO LocalizedText
+		(Language,	Tag,														Text)
+VALUES	('pt_BR',	'LOC_LOAD_CONFIG_REGENERATE_SEED_WARNING',					'Deseja gerar uma seed nova para esse jogo?.');
